57d7680cd2cc96d202790757b7cbcd1ceef17223
omidahomi <oiqbal@sjnma.org>
2017-05-10 11:00:31 -0500

    [almost done]

---
+++ extracredit.py
@@ -0,0 +1,12 @@
+def OddOdd(values):
+	Toggle= True
+	SumToReturn= 0
+	for value in values:
+		if(Toggle == True):
+			if(values%2 == 0):
+				SumToReturn= SumToReturn + value
+	Toggle==NotToggle
+	return SumToReturn
+
+
+stud= (([]))
